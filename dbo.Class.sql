fad58d908ac958ed8074ebdd9b75f1b0e1da4e8a
Zyprush <100828576+Zyprush@users.noreply.github.com>
2022-03-31 14:57:19 +0900

    Add files via upload

---
+++ dbo.Class.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Class] (
+    [ID]      INT            IDENTITY (1, 1) NOT NULL,
+    [Name]    NVARCHAR (MAX) NULL,
+    [Age]     NVARCHAR (MAX) NULL,
+    [Gender]  NVARCHAR (MAX) NULL,
+    [Address] NVARCHAR (MAX) NULL,
+    [Contact] FLOAT (53)     NOT NULL,
+    CONSTRAINT [PK_Class] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
